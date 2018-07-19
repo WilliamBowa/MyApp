dc7f07e4f9cfdff1ce2dbee1c93152039d6889a9
william bowa <william.bowa@mediapro.com>
2018-07-19 14:44:39 -0700

    Seeded Database with sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+    Blog.create!(
+        title: "My Blog Post #{blog}",
+        body: " At vero eos et accusamus et iusto odio dignissimos ducimus qui 
+                blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias 
+                excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, 
+                id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, 
+                cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis 
+                voluptas assumenda est, omnis dolor repellendus. "
+    )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+      title: "Rails #{skill}",
+      percent_utilized: 15
+  )
+end
+
+puts "5 skills created"
+
+9.times do |portofolio_item|
+    Portfolio.create!(
+        title: "Portfolio title #{portofolio_item}",
+        subtitle: "my great service",
+        body: " Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
+                eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+        main_image: "https://placeholdit.co//i/600x400",
+        thumb_image: "https://placeholdit.co//i/350x200"
+    )
+  end
