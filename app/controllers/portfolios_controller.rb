fcfe1c50746f407400cfae96e74c423f5530d4b9
william bowa <william.bowa@mediapro.com>
2018-07-19 17:30:38 -0700

    Added portfolio feature index/new/create file

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,24 @@
 class PortfoliosController < ApplicationController
+    def index
+        @portfolio_items = Portfolio.all
+    end
+
+    #Create action to create a new instance of class portfolio make availlable to form
+    def new
+        @portfolio_item = Portfolio.new
+    end
+
+    #Create action connect the form new record into the database
+    def create
+        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    
+        respond_to do |format|
+          if @portfolio_item.save
+            format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+            format.json { render :show, status: :created, location: @portfolio_item }
+          else
+            format.html { render :new }
+          end
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -36,7 +36,7 @@ puts "5 skills created"
         body: " Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
                 eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
                 Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
-        main_image: "https://placeholdit.co//i/600x400",
+        main_image: "https://placeholdit.co//i/300x200",
         thumb_image: "https://placeholdit.co//i/350x200"
     )
   end
