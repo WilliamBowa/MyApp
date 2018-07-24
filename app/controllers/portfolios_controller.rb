e1957aac8b240fd109e3273e44c93d196f1da6b6
william bowa <william.bowa@mediapro.com>
2018-07-24 11:04:24 -0700

    implemented show file and destroy action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -34,12 +34,28 @@ class PortfoliosController < ApplicationController
         
         respond_to do |format|
           if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-            format.html { redirect_to @portfolios_path, notice: 'Portfolio was successfully updated.' }
+            format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
             format.json { render :show, status: :created, location: @portfolio_item }
-            
           else
             format.html { render :edit }
           end
+        end
+    end
+
+    def show
+        @portfolio_item = Portfolio.find(params[:id])
+    end
+
+    def destroy
+        # Peform the lookup
+        @portfolio_item = Portfolio.find(params[:id])
+
+        #Destroy/delete the record
+        @portfolio_item.destroy
+
+        # Redirect
+        respond_to do |format|
+            format.html { redirect_to portfolios_url, notice: 'Item was successfully destroyed.' }
         end
     end
 end
