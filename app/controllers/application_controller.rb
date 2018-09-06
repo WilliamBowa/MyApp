823ba117d6512fb893c4908373ccb6a96b8fd41e
william bowa <william.bowa@mediapro.com>
2018-09-05 19:28:56 -0700

    generate view for device

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # before_filter :configure_permitted_parameters, if: :device_controller?
+  
+  # ## Allow name attribute of device to be saved from UI
+  # def configure_permitted_parameters
+  #   device_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  #   device_parameter_sanitizer.permit(:account_update, keys: [:name])
+  # end
 end
